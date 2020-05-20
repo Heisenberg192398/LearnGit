8097e367b98dbfc951b777fb6aa322141cc20095
Heisenberg192398 <tomarshreyansh.237@gmail.com>
2020-05-20 16:07:08 +0530

    FOR LOOP

---
+++ DAY-2.py
@@ -0,0 +1,45 @@
+# for i in range(2,10):
+#     print(i)
+# for i in range(2,10,2):
+#     print(i)
+# for i in range(10,1,-3):
+#     print(i)
+# sum = 0
+# n = int(input())
+# for i in range(n+1):
+#     # sum = sum =i
+#     sum+=i
+
+# print(sum)
+# sum = 0
+# n = int(input())
+# i=0
+# while()
+# sum = 0
+# for i in range(1,6):
+#      cube=i**3
+#      sum+=cube
+# print(cube)
+    # """ break statement"""
+# i = 0
+# while i<= 10:
+#     if i==6:
+#         break
+#     i+=1
+#     print(i)
+
+# print(i)
+# """ infinite loop"""
+# i = 1
+# while(i<10):
+#     if(i==5):
+#         continue
+#     print(i)
+#     i+=1
+ i=0
+ while(i<10):
+     if (i==5)
+     i += 1
+     continue
+
+    print(i)
